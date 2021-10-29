55b8978ec1f2c564a85b68d3c407d8ba111448be
Alejandro <azunigapaz@gmail.com>
2021-10-28 21:34:40 -0600

    Se creo Activity Consulta Personas
    No se ha hecho nada de la parte logica, solo disenio

---
+++ app/src/main/java/com/grupoadec/pm1ejercicio1_3/ActivityConsultaPersonas.java
@@ -0,0 +1,14 @@
+package com.grupoadec.pm1ejercicio1_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityConsultaPersonas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_personas);
+    }
+}
